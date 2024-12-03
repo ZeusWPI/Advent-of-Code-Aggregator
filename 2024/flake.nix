279a5cfa7cd9e5c925ed3fb2c0f727bcfc912b4c
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-12-03 23:46:09 +0100

    Nix flake :eyes:

---
+++ 2024/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Zeus AoC 2024";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "Advent of Code 2024";
+          packages = [
+            pkgs.hyperfine
+            pkgs.postgresql_14
+            pkgs.ldc
+            pkgs.go
+          ];
+        };
+      }
+    );
+}
